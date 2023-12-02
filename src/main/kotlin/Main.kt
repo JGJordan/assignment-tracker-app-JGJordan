d46203977d3a956fe9388b9a540dfbcb1255672b
JGJordan <20106285@mail.wit.ie>
2023-12-02 17:16:17 +0000

    Add Assignment data class in models

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,37 @@
 package utils
+
 import java.util.*
 import java.lang.System.exit
 
-//Main
+/**
+ * Runs the assignment tracker menu.
+ */
 fun main(args: Array<String>) {
     runMenu()
 }
 
-//Functions
-fun mainMenu() : Int {
+/**
+ * Displays the main menu options and retrieves user input.
+ * @return The selected menu option.
+ */
+fun mainMenu(): Int {
     return ScannerInput.readNextInt("""
-|    ____________________________
-|    | Assignment Tracker V1.0  | 
-|    |--------------------------|
-|    | 1) View Subjects         |                 
-|    | 2) Add Subject           |
-|    | 3) Update Subject        |
-|    | 4) Delete Subject        |
-|    |--------------------------|       
-|    | 0) Exit Program          |
-|    |--------------------------|
+        |    ____________________________
+        |    | Assignment Tracker V1.0  |
+        |    |--------------------------|
+        |    | 1) View Subjects         |
+        |    | 2) Add Subject           |
+        |    | 3) Update Subject        |
+        |    | 4) Delete Subject        |
+        |    |--------------------------|
+        |    | 0) Exit Program          |
+        |    |--------------------------|
     """.trimMargin())
 }
 
+/**
+ * Runs the main menu loop.
+ */
 fun runMenu() {
     do {
         val option = mainMenu()
@@ -34,23 +43,41 @@ fun runMenu() {
             0 -> closeApp()
             else -> println("Option: $option is not valid. Try again")
         }
-    } while(true)
+    } while (true)
 }
 
-fun viewSubject(){
+/**
+ * Displays a message indicating the user chose to view a subject.
+ */
+fun viewSubject() {
     println("You chose to view a subject")
 }
-fun addSubject(){
+
+/**
+ * Displays a message indicating the user chose to add a subject.
+ */
+fun addSubject() {
     println("You chose to add a subject")
 }
-fun updateSubject(){
+
+/**
+ * Displays a message indicating the user chose to update a subject.
+ */
+fun updateSubject() {
     println("You chose to update a subject")
 }
-fun deleteSubject(){
+
+/**
+ * Displays a message indicating the user chose to delete a subject.
+ */
+fun deleteSubject() {
     println("You chose to delete a subject")
 }
-fun closeApp(){
+
+/**
+ * Displays a message indicating the user chose to exit the program and closes the application.
+ */
+fun closeApp() {
     println("You chose to exit the program")
     exit(0)
 }
-
---
+++ src/main/kotlin/models/Assignment.kt
@@ -0,0 +1,7 @@
+package utils.models
+
+data class Assignment(val subjectID: Int,
+                      val subjectName : String,
+                      val subjectGrade : Double,
+                      val subjectLecturer : String
+    )
