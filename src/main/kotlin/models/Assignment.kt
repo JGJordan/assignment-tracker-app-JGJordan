5b79bcc9a9373deb26a1c8b93907c365a2261a4a
JGJordan <20106285@mail.wit.ie>
2023-12-02 17:41:59 +0000

    Add NoteAPI to handle array list + add subject function + add logging

--- src/main/kotlin/models/Assignment.kt
+++ src/main/kotlin/models/Assignment.kt
@@ -1,6 +1,6 @@
 package utils.models
 
-data class Assignment(val subjectID: Int,
+data class Subject(val subjectID: Int,
                       val subjectName : String,
                       val subjectGrade : Double,
                       val subjectLecturer : String
