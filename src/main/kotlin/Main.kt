abe277f4e2d37e2672646aba42d37de86d1daae0
JGJordan <20106285@mail.wit.ie>
2023-12-02 13:26:37 +0000

    Add menu with scanner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,27 @@
+import java.util.*
+
+//Variables
+val scanner = Scanner(System.`in`)
+
+//Main
 fun main(args: Array<String>) {
 }
+
+//Functions
+fun mainMenu() : Int {
+    print("""
+|    ____________________________
+|    | Assignment Tracker V1.0  | 
+|    |--------------------------|
+|    | 1) View Subjects         |                 
+|    | 2) Add Subject           |
+|    | 3) Update Subject        |
+|    | 4) Delete Subject        |
+|    |--------------------------|       
+|    | 0) Exit Program          |
+|    |--------------------------|
+    """.trimMargin())
+    return scanner.nextInt()
+}
+
+
