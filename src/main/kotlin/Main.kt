da792a81f6bf40aa88bbb27a103357558182b821
JGJordan <20106285@mail.wit.ie>
2023-12-02 15:45:25 +0000

    Add menu and corresponding test function + ScannerInput package

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
+package utils
 import java.util.*
-
-//Variables
-val scanner = Scanner(System.`in`)
+import java.lang.System.exit
 
 //Main
 fun main(args: Array<String>) {
+    runMenu()
 }
 
 //Functions
 fun mainMenu() : Int {
-    print("""
+    return ScannerInput.readNextInt("""
 |    ____________________________
 |    | Assignment Tracker V1.0  | 
 |    |--------------------------|
@@ -21,7 +21,36 @@ fun mainMenu() : Int {
 |    | 0) Exit Program          |
 |    |--------------------------|
     """.trimMargin())
-    return scanner.nextInt()
 }
 
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1 -> viewSubject()
+            2 -> addSubject()
+            3 -> updateSubject()
+            4 -> deleteSubject()
+            0 -> closeApp()
+            else -> println("Option: $option is not valid. Try again")
+        }
+    } while(true)
+}
+
+fun viewSubject(){
+    println("You chose to view a subject")
+}
+fun addSubject(){
+    println("You chose to add a subject")
+}
+fun updateSubject(){
+    println("You chose to update a subject")
+}
+fun deleteSubject(){
+    println("You chose to delete a subject")
+}
+fun closeApp(){
+    println("You chose to exit the program")
+    exit(0)
+}
 
